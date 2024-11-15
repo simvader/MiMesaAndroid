d76a22e2b68564dfc852233f5881262658e7c884
simvader <simbadfernandes@gmail.com>
2024-11-15 12:34:08 -0300

    refactor

--- app/src/main/java/com/example/mimesa/ui/composables/MenuScreen.kt
+++ app/src/main/java/com/example/mimesa/ui/composables/MenuScreen.kt
@@ -3,23 +3,13 @@ package com.example.mimesa.ui.composables
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.navigation.NavController
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MenuScreen(
     categories: List<String>,
